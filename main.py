6dbe9a733e91b65aa368e11e886073d305bf6e60
Setio Wahono <setiowahono@gmail.com>
2022-11-10 11:18:17 +0700

    .

---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import pandas as pd
+import datetime
+
+url = 'https://gql.tokopedia.com/graphql/SearchProductQueryV4'
+cari = 'iPhone 14 Pro'
+
+def get_params():
+    params = []
+    for i in range(1, 5):
+        param = 'device=desktop&navsource=&ob=23&page={}&q={}&related=true&rows=60&safe_search=false&scheme=https&shipping=&source=search&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&st=product&start={}&topads_bucket=true&unique_id=3a31b8de3343a19ce95f2544ccc83620&user_addressId=&user_cityId=176&user_districtId=2274&user_id=&user_lat=&user_long=&user_postCode=&user_warehouseId=12210375&variants='.format(
+            i, cari, (i-1)*60)
+        params.append(param)
+    return params
+
+def scrape_data(param):
+    payload = [
+        {
+            'operationName': 'SearchProductQueryV4',
+            'variables': {
+                'params': param
+            },
+            'query': 'query SearchProductQueryV4($params: String!) {\n  ace_search_product_v4(params: $params) {\n    header {\n      totalData\n      totalDataText\n      processTime\n      responseCode\n      errorMessage\n      additionalParams\n      keywordProcess\n      componentId\n      __typename\n    }\n    data {\n      banner {\n        position\n        text\n        imageUrl\n        url\n        componentId\n        trackingOption\n        __typename\n      }\n      backendFilters\n      isQuerySafe\n      ticker {\n        text\n        query\n        typeId\n        componentId\n        trackingOption\n        __typename\n      }\n      redirection {\n        redirectUrl\n        departmentId\n        __typename\n      }\n      related {\n        position\n        trackingOption\n        relatedKeyword\n        otherRelated {\n          keyword\n          url\n          product {\n            id\n            name\n            price\n            imageUrl\n            rating\n            countReview\n            url\n            priceStr\n            wishlist\n            shop {\n              city\n              isOfficial\n              isPowerBadge\n              __typename\n            }\n            ads {\n              adsId: id\n              productClickUrl\n              productWishlistUrl\n              shopClickUrl\n              productViewUrl\n              __typename\n            }\n            badges {\n              title\n              imageUrl\n              show\n              __typename\n            }\n            ratingAverage\n            labelGroups {\n              position\n              type\n              title\n              url\n              __typename\n            }\n            componentId\n            __typename\n          }\n          componentId\n          __typename\n        }\n        __typename\n      }\n      suggestion {\n        currentKeyword\n        suggestion\n        suggestionCount\n        instead\n        insteadCount\n        query\n        text\n        componentId\n        trackingOption\n        __typename\n      }\n      products {\n        id\n        name\n        ads {\n          adsId: id\n          productClickUrl\n          productWishlistUrl\n          productViewUrl\n          __typename\n        }\n        badges {\n          title\n          imageUrl\n          show\n          __typename\n        }\n        category: departmentId\n        categoryBreadcrumb\n        categoryId\n        categoryName\n        countReview\n        customVideoURL\n        discountPercentage\n        gaKey\n        imageUrl\n        labelGroups {\n          position\n          title\n          type\n          url\n          __typename\n        }\n        originalPrice\n        price\n        priceRange\n        rating\n        ratingAverage\n        shop {\n          shopId: id\n          name\n          url\n          city\n          isOfficial\n          isPowerBadge\n          __typename\n        }\n        url\n        wishlist\n        sourceEngine: source_engine\n        __typename\n      }\n      violation {\n        headerText\n        descriptionText\n        imageURL\n        ctaURL\n        ctaApplink\n        buttonText\n        buttonType\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n'
+        }
+    ]
+    req = requests.post(url, json=payload).json()
+    rows = req[0]['data']['ace_search_product_v4']['data']['products']
+    # print(len(rows))
+    scrape_data = []
+    for i in range(0, len(rows)):
+        # no = i
+        toko = rows[i]['shop']['name']
+        lokasi = rows[i]['shop']['city']
+        nama_produk = rows[i]['name']
+        harga = rows[i]['price'].replace('Rp', '')
+        rating = rows[i]['ratingAverage']
+        # print(no, nama_produk, harga, rating, toko, lokasi)
+        scrape_data.append(
+            (toko, lokasi, nama_produk, harga, rating)
+        )
+    return scrape_data
+
+if __name__ == '__main__':
+    params = get_params()
+    all_data = []
+    for i in range(1, len(params)):
+        param = params[i]
+        data = scrape_data(param)
+        all_data.extend(data)
+
+df = pd.DataFrame(data, columns=['Nama Toko', 'Lokasi', 'Nama Barang', 'Harga', 'Rating'])
+now = datetime.datetime.now()
+df.to_excel(f'tokopedia_api-{now.strftime("%Y%m%d%H%M%S")}.xlsx', index = False)
+print('Done')
+
